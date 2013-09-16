bb98d844f373f77849a4fb3c98192555e0777538
Bruno Gama Catão <brunogamacatao@gmail.com>
2013-09-16 18:15:44 -0300

    Fixed one query

--- app/controllers/cursos_controller.rb
+++ app/controllers/cursos_controller.rb
@@ -2,7 +2,7 @@ class CursosController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    @matriculas = Matricula.where("user_id = ?", current_user).order("created_at DESC")
+    @matriculas = Matricula.where("user_id = ? AND estado_cd = ?", current_user, Matricula.estados(:confirmada)).order("created_at DESC")
   end
   
   def show
