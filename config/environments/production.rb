b372011837857c195185fbfc1f2d999641e7d16d
Bruno Gama Catão <brunogamacatao@gmail.com>
2013-09-27 21:03:29 -0300

    trying to fix mailer errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,8 @@ Blog::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_options = { from: "admin@brunocatao.com" }
+  config.action_mailer.default_url_options = { :host => "brunocatao.com" }
 
   # Enable threaded mode
   # config.threadsafe!
