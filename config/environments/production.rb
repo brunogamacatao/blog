b888e1c237c4d72c18cf023137cfbe2ae0279f00
Bruno Gama Catão <brunogamacatao@gmail.com>
2013-09-27 21:35:45 -0300

    Trying to fix the action mailer bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Blog::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.default_options = { from: "admin@brunocatao.com" }
+  config.action_mailer.default_options = { from: "admin@brunocatao.com" }
   # config.action_mailer.default_url_options = { :host => "brunocatao.com" }
 
   # Enable threaded mode
