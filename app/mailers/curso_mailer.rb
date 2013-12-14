ae35f152c756ebdeea7b9954f4ef861aacf3c25e
Bruno Catao <brunogamacatao@gmail.com>
2013-12-13 22:09:27 -0300

    Agora vai

--- app/mailers/curso_mailer.rb
+++ app/mailers/curso_mailer.rb
@@ -1,9 +1,14 @@
+#encoding: utf-8
+
 class CursoMailer < ActionMailer::Base
   default from: "admin@brunocatao.com"
   
   def mensagem(matricula, mensagem)
     @matricula = matricula
     @mensagem  = mensagem
-    mail to: matricula.user.email
+    
+    print "Enviando mensagem para: #{matricula.user.email}"
+    
+    mail(to: matricula.user.email, subject: "Nova mensagem para você")
   end
 end
